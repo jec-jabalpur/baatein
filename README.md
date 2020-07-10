1df2e84b5c398104f24a82033d1cb50f50ad64a1
unmesh-shukla <unmesh.shukla.personal@gmail.com>
2020-07-10 23:11:13 +0530

    Added initial migrations, registration and login features, requirements.txt, .gitignore and Setup Instructions

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # baatein
-Class Project for College students - Python Training
+Python project for college students
+
+## Setup
+
+- Clone the repository
+    ```
+    git clone https://github.com/jec-jabalpur/baatein
+    ```
+
+- Switch to the project directory
+    ```
+    cd baatein
+    ```
+
+- Create a Virtual Environment and activate it
+    ```
+    python -m venv venv
+    source venv/bin/activate (for Linux)
+    venv/Scripts/activate (for Windows)
+    ```
+
+- Install all packages
+    ```
+    pip install -r requirements.txt
+    ```
+
+- Set the FLASK_APP environment variable if .flaskenv is not working for you.
+    ```
+    export FLASK_APP=baatein.py (for Linux)
+    set FLASK_APP=baatein.py (for Windows)
+    ```
+
+- Run the DB migrations
+    ```
+    flask db init
+    flask db upgrade
+
+    ```
+
+- Run the application
+    ```
+    flask run
+    ```
+
+Open http://localhost:5000 to view the working application.
--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
+from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(120), unique=True)
@@ -8,3 +14,25 @@ class User(db.Model):
 
     def __repr__(self):
         return '(User {})'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '(Post {})'.format(self.body)
+
