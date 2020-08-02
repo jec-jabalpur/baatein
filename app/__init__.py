dc5d74609a205ec96e22f2701ac46b0eae1066a6
unmesh-shukla <unmesh.shukla.personal@gmail.com>
2020-08-02 16:03:18 +0530

    Client-side timestamp management

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from config import Config
 import logging
 from logging.handlers import SMTPHandler
@@ -23,6 +24,8 @@ login.login_view = 'login'
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 from app import routes, models, errors
 
